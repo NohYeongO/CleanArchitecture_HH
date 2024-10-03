0dad8ab1b776f4b9526f6225ee6dcaed48f3bd24
노영오 <nohyo05@naver.com>
2024-10-04 02:22:57 +0900

    Initial commit

---
+++ src/main/java/io/hhplus/hh_cleanarchitecture/HhCleanArchitectureApplication.java
@@ -0,0 +1,13 @@
+package io.hhplus.hh_cleanarchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhCleanArchitectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HhCleanArchitectureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/hhplus/hh_cleanarchitecture/HhCleanArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hhplus.hh_cleanarchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhCleanArchitectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
